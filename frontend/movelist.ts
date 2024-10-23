a65d158b7f984224a750d5ddc7dbaa031b4a2f82
Alexander Lyashuk <mooskagh@gmail.com>
2024-10-23 15:16:54 +0200

    Click click -- move selection.

--- frontend/movelist.ts
+++ frontend/movelist.ts
@@ -1,4 +1,3 @@
-
 export interface GamePositionResponse {
   ply: number;  // 0 for startpos
   thinkingId?: number;
@@ -13,12 +12,46 @@ export interface GamePositionResponse {
   scoreL?: number;
 }
 
+export interface MoveSelectionObserver {
+  onMoveSelected(position: GamePositionResponse): void;
+}
+
 export class MoveList {
   private element: HTMLElement;
   private positions: GamePositionResponse[] = [];
+  private positionIdx: number = 0;
+  private observers: MoveSelectionObserver[] = [];
 
   constructor(element: HTMLElement) {
     this.element = element;
+    this.element.addEventListener('click', (event: Event) => {
+      const target = event.target as HTMLElement;
+      const plyIdx = target.getAttribute('ply-idx');
+      if (plyIdx !== null) this.selectPly(parseInt(plyIdx, 10));
+    });
+  }
+
+  public addObserver(observer: MoveSelectionObserver): void {
+    this.observers.push(observer);
+  }
+  public removeObserver(observer: MoveSelectionObserver): void {
+    this.observers = this.observers.filter(o => o !== observer);
+  }
+
+  private notifyObservers(): void {
+    this.observers.forEach(
+        observer => observer.onMoveSelected(this.positions[this.positionIdx]));
+  }
+
+  private selectPly(positionIdx: number): void {
+    Array.from(this.element.children)
+        .forEach(row => row.classList.remove('movelist-selected'));
+    const targetRow =
+        Array.from(this.element.children)
+            .find(row => row.getAttribute('ply-idx') === String(positionIdx));
+    targetRow?.classList.add('movelist-selected');
+    this.positionIdx = positionIdx;
+    this.notifyObservers();
   }
 
   public setPositions(positions: GamePositionResponse[]): void {
@@ -30,10 +63,13 @@ export class MoveList {
       const is_black = (position.ply % 2) === 0;
 
       const rowEl = document.createElement('div');
+      rowEl.classList.add('movelist-item');
+      rowEl.setAttribute('ply-idx', position.ply.toString());
       rowEl.innerHTML =
           `${is_black ? '&nbsp;&nbsp;&nbsp;…' : `${move_idx}. `} ${
               position.moveSan}`;
       this.element.appendChild(rowEl);
     });
+    this.selectPly(this.positions.length - 1);
   }
 };
